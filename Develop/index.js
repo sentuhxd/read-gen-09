d127fadeffabb25342a7a1a0d2d556466b189b1c
sentuhxd <142236429+sentuhxd@users.noreply.github.com>
2023-09-14 11:13:26 -0400

    added dependencies

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,12 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown.js')
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
